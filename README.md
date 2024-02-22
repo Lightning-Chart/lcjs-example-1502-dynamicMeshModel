d0ebb581abf1563908ad84285768922d73b7d84d
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-02-22 14:22:20 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,66 @@
+# Dynamic coloring of 3D Mesh Model.
+
+![Dynamic coloring of 3D Mesh Model.](dynamicMeshModel-darkGold.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
+
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
+
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+This example demonstrates how to visualize 3D mesh models with customized coloring using the setVertexValues() method and PalettedFill.
+
+To apply color to your 3D model using PalettedFill, you'll need to set values for each vertex.
+
+First, every vertex of the model is iterated, and a weight for every sensor is calculated for that vertex.
+
+This weight essentially describes how close to the vertex every sensor is.
+
+Then, the sensor values are changed in real-time, and the vertex values are re-interpolated every time they do to update the colors.
+
+This can be achieved by calling the setVertexValues() method, which accepts a callback function as an argument. This callback function should take a vertex as a parameter and return an array of colored values based on your criteria.
+
+![distance to each vertices](./assets/schema.png)
+
+
+## API Links
+
+* [Mesh Model]
+* [Vertex Values]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [LightningChart][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact sales@lightningchart.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
+
+[0]: https://github.com/Arction/
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://lightningchart.com/support-services/
+
+© LightningChart Ltd 2009-2022. All rights reserved.
+
+
+[Mesh Model]: https://lightningchart.com/js-charts/api-documentation/v5.1.0/classes/MeshModel3D.html
+[Vertex Values]: https://lightningchart.com/js-charts/api-documentation/v5.1.0/classes/MeshModel3D.html#setVertexValues
+
---
+++ src/index.js
@@ -0,0 +1,158 @@
+/*
+ * Histogram of world population by age per year.
+ */
+
+const lcjs = require('@arction/lcjs')
+const obj = require('webgl-obj-loader')
+
+const { lightningChart, PalettedFill, ColorRGBA, AxisTickStrategies, LUT, emptyFill, Themes } = lcjs
+
+const chart = lightningChart()
+    .Chart3D({
+        // theme: Themes.darkGold,
+    })
+    .setBoundingBox({ x: 0.7, y: 0.5, z: 1 })
+    .setTitle('Real-Time Airplane Temperature')
+
+chart
+    .getDefaultAxes()
+    .forEach((axis) =>
+        axis.setTickStrategy(AxisTickStrategies.Numeric, (ticks) =>
+            ticks
+                .setMajorTickStyle((major) => major.setLabelFillStyle(emptyFill))
+                .setMinorTickStyle((minor) => minor.setLabelFillStyle(emptyFill)),
+        ),
+    )
+
+fetch(document.head.baseURI + 'examples/assets/1502/air.obj')
+    .then((response) => response.text())
+    .then((data) => {
+        const modelParsed = new obj.Mesh(data)
+
+        const model = chart
+            .addMeshModel()
+            .setScale(0.0025)
+            .setModelGeometry({ vertices: modelParsed.vertices, indices: modelParsed.indices, normals: modelParsed.vertexNormals })
+            .setHighlightOnHover(false)
+            .setName('Airplane')
+
+        const palette = new PalettedFill({
+            lookUpProperty: 'value',
+            lut: new LUT({
+                units: '°C',
+                interpolate: true,
+                steps: [
+                    { value: 0, color: ColorRGBA(0, 150, 255) },
+                    { value: 20, color: ColorRGBA(0, 255, 0) },
+
+                    { value: 40, color: ColorRGBA(200, 255, 0) },
+                    { value: 50, color: ColorRGBA(255, 255, 0) },
+                    { value: 60, color: ColorRGBA(255, 200, 0) },
+
+                    { value: 100, color: ColorRGBA(255, 130, 0) },
+                    { value: 120, color: ColorRGBA(255, 0, 0) },
+                ],
+            }),
+        })
+
+        model.setFillStyle(palette)
+
+        const sensors = [
+            // Engines close to the body
+            { initValue: 90, value: 100, x: -0.252, y: -0.175, z: -0.25 },
+            { initValue: 90, value: 100, x: 0.252, y: -0.175, z: -0.25 },
+
+            // Engines far from the body
+            { initValue: 110, value: 100, x: -0.52, y: -0.145, z: -0.07 },
+            { initValue: 110, value: 100, x: 0.52, y: -0.145, z: -0.07 },
+
+            // Nose of the plane
+            { initValue: 20, value: 20, x: 0, y: -0.1, z: -1 },
+
+            // Back of the plane
+            { initValue: 50, value: 50, x: 0, y: -0.04, z: 0.97 },
+
+            // Body ?
+            { initValue: 20, value: 20, x: 0, y: -0.1, z: -0.5 },
+            { initValue: 20, value: 20, x: 0, y: -0.1, z: 0 },
+            { initValue: 0, value: 0, x: 0, y: -0.1, z: 0.5 },
+        ]
+
+        // Skip first frame to avoid possible initial lag
+        requestAnimationFrame(() => {
+            // Initialize an empty array to store information about each vertex's relationship with sensors.
+            const vertexCoordSensorWeights = []
+
+            // Set vertex values using a callback function.
+            model.setVertexValues((coordsWorld) => {
+                const vertexValues = []
+
+                // Loop through the world coordinates of each vertex.
+                for (let i = 0; i < coordsWorld.length; i += 1) {
+                    // Convert the vertex's world coordinates into axis coordinates.
+                    const locAxis = chart.translateCoordinate(coordsWorld[i], chart.coordsWorld, chart.coordsAxis)
+
+                    // Create an array to store sensor weights.
+                    const sensorWeights = new Array(sensors.length).fill(0)
+                    let sumOfWeights = 0
+
+                    // Calculate distances and weights for each sensor.
+                    sensors.forEach((sensor, i2) => {
+                        const locationDeltaX = sensor.x - locAxis.x
+                        const locationDeltaY = sensor.y - locAxis.y
+                        const locationDeltaZ = sensor.z - locAxis.z
+                        const dist = Math.sqrt(locationDeltaX ** 2 + locationDeltaY ** 2 + locationDeltaZ ** 2)
+                        const weight = dist !== 0 ? 1 / dist ** 3 : 1
+                        sensorWeights[i2] = weight
+                        sumOfWeights += weight
+                    })
+
+                    // Store sum of weights and sensor weights for each vertex.
+                    vertexCoordSensorWeights.push({ sumOfWeights, sensorWeights })
+
+                    // Calculate vertex value based on sensor values and weights.
+                    const vertexValue = sensors.reduce((prev, cur, i2) => prev + cur.value * sensorWeights[i2], 0) / sumOfWeights || 20
+
+                    // Push the vertex value to the array.
+                    vertexValues.push(vertexValue)
+                }
+
+                // Return the array of vertex values.
+                return vertexValues
+            })
+
+            // Animation frame function.
+            const frame = () => {
+                // Update sensor values for animation.
+                sensors.forEach((sensor, i) => {
+                    sensor.value = sensor.initValue + 10 * Math.sin(window.performance.now() / 500)
+                })
+
+                const vertexValues = []
+
+                // Update vertex values using a callback function.
+                model.setVertexValues((vertex) => {
+                    for (let i = 0; i < vertex.length; i += 1) {
+                        const { sumOfWeights, sensorWeights } = vertexCoordSensorWeights[i]
+
+                        // Calculate vertex value based on sensor values and weights.
+                        const vertexValue = sensors.reduce((prev, cur, i2) => prev + cur.value * sensorWeights[i2], 0) / sumOfWeights || 20
+
+                        // Push the vertex value to the array.
+                        vertexValues.push(vertexValue)
+                    }
+
+                    // Return the updated array of vertex values.
+                    return vertexValues
+                })
+
+                // Request the next animation frame.
+                requestAnimationFrame(frame)
+            }
+
+            // Start the animation.
+            frame()
+        })
+
+        const legend = chart.addLegendBox().add(chart)
+    })
